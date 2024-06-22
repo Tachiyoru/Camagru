b46589054691a5a65131b9b260139d33ed7f7b33
Shanley LEON <shanley@hotmail.fr>
2024-06-22 16:53:52 +0200

    [fixed css + script + login work]

---
+++ public/js/scripts.js
@@ -0,0 +1,31 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+  event.preventDefault(); // Empêcher le formulaire de soumettre normalement
+
+  console.log('Form submitted!');
+  const formData = new FormData(event.target);
+
+  fetch('/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    },
+    body: new URLSearchParams(formData)
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Authentication failed');
+    }
+    document.getElementById('loading-screen').classList.add('active');
+    return response.json();
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    window.location.href = '/';
+  })
+  .finally(() => {
+    var loadingTime = Math.random() * (4000 - 2000) + 2000;
+    setTimeout(() => {
+    window.location.href = '/homepage';
+  }, loadingTime); 
+  });
+});
--- src/server.js
+++ src/server.js
@@ -1,54 +1,77 @@
-const http = require('http');
-const fs = require('fs');
-const mongoose = require('mongoose')
-const router = require('./routes');
-require('dotenv').config();
+const http = require("http");
+const fs = require("fs");
+const mongoose = require("mongoose");
+const router = require("./routes");
+require("dotenv").config();
 
 const MG_DB_PSWRD = process.env.MG_DB_PSWRD;
 const URI = `mongodb+srv://tachiyoru:${MG_DB_PSWRD}@camagrucluster.5s5jmag.mongodb.net/?retryWrites=true&w=majority&appName=CamagruCluster`;
 
 async function connect() {
-  try{
-    await mongoose.connect(URI)
-    console.log('Connected to DB')
+  try {
+    await mongoose.connect(URI);
+    console.log("Connected to DB");
   } catch (err) {
-    console.error(err)
+    console.error(err);
   }
 }
 
-const path = require('path');
+const path = require("path");
 
 const requestHandler = (req, res) => {
   const url = req.url;
 
-  if (url === '/') {
-    fs.readFile(path.join(__dirname, '../public/index.html'), (err, data) => {
+  if (url === "/") {
+    fs.readFile(path.join(__dirname, "../public/index.html"), (err, data) => {
       if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
         return;
       }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.end(data);
     });
-  } else if (url === '/homepage') {
-    fs.readFile(path.join(__dirname, '../public/Homepage.html'), (err, data) => {
+  } else if (url.match(/\.css$/)) {
+    fs.readFile(path.join(__dirname, "../public", url), (err, data) => {
       if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found");
         return;
       }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.writeHead(200, { "Content-Type": "text/css" });
       res.end(data);
     });
-  } else if (url === '/loading') {
-    fs.readFile(path.join(__dirname, '../public/gallery.css'), (err, data) => {
+  } else if (url.match(/\.js$/)) {
+    fs.readFile(path.join(__dirname, "../public", url), (err, data) => {
       if (err) {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end('Internal Server Error');
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        res.end("Not Found");
         return;
       }
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.writeHead(200, { "Content-Type": "application/javascript" });
+      res.end(data);
+    });
+  } else if (url === "/homepage") {
+    fs.readFile(
+      path.join(__dirname, "../public/Homepage.html"),
+      (err, data) => {
+        if (err) {
+          res.writeHead(500, { "Content-Type": "text/plain" });
+          res.end("Internal Server Error");
+          return;
+        }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    );
+  } else if (url === "/loading") {
+    fs.readFile(path.join(__dirname, "../public/gallery.css"), (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+        return;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
       res.end(data);
     });
   } else {
